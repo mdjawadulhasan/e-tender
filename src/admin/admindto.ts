69f89e2335986cb57144d2ac5ad070ec244e01c0
Md. Jawadul Hasan <114417916+Tasnova-Apon@users.noreply.github.com>
2023-02-12 12:29:32 +0600

    Lab Task 2 Done

--- src/admin/admindto.ts
+++ src/admin/admindto.ts
@@ -1,7 +1,9 @@
-import { IsEmail, IsNotEmpty } from "class-validator";
+import { IsByteLength, IsEmail, IsNotEmpty, MaxLength, MinLength } from "class-validator";
 
 export class AdminForm {
     @IsNotEmpty({ message: "Please enter your Id" })
+    @MinLength(2, { message: "ID should be at least 2 characters long" })
+    @MaxLength(10, { message: "ID should not be longer than 10 characters" })
     id: number;
 
     @IsNotEmpty({ message: "Please enter your Name" })
@@ -10,6 +12,8 @@ export class AdminForm {
     @IsEmail()
     email: string
 
-
+    @IsNotEmpty({ message: "Please enter your phone number" })
+    @IsByteLength(10)
+    phoneNumber: string;
 
 }
