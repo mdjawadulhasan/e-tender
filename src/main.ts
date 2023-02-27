40c4e33778da30730d3fa41b40e39c2170c1ca09
Md. Jawadul Hasan <66155771+mdjawadulhasan@users.noreply.github.com>
2023-02-28 02:57:32 +0600

    Main ts

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(
-    session({
-      secret: 'my-secret',
-      resave: false,
-      saveUninitialized: false,
-    }),)
   await app.listen(3000);
 }
 bootstrap();
