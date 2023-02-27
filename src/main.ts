d8428c64309c3f4024897aa7d9e0fe642f93c43b
Md. Jawadul Hasan <66155771+mdjawadulhasan@users.noreply.github.com>
2023-02-28 02:28:24 +0600

    Session ongoing

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import session from 'express-session';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    session({
+      secret: 'my-secret',
+      resave: false,
+      saveUninitialized: false,
+    }),)
   await app.listen(3000);
 }
 bootstrap();
