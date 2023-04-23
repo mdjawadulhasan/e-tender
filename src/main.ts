3bb46aa547765f3f210a373acbaf34ec30cd798b
jawadbs23 <66155771+mdjawadulhasan@users.noreply.github.com>
2023-04-23 19:18:26 +0600

    CORS eddited

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
-import * as session from 'express-session';
+import { ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import * as session from 'express-session';
+import * as cors from 'cors';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter()
+  );
+
+  // Enable CORS middleware
+  app.use(cors({
+    origin: 'http://localhost:8001', // Set the domain of your NextJS app
+    credentials: true, // Enable credentials such as cookies and authorization headers
+  }));
+
+  // Enable session middleware
   app.use(
     session({
       secret: 'my-secret',
@@ -14,6 +27,7 @@ async function bootstrap() {
       }
     }),
   );
+
   await app.listen(3000);
 }
 bootstrap();
