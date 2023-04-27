70dc3524a13dfed3cd00a3cdbf334d1625b34ce6
JoyMatubber <joy.matubber.41959@gmail.com>
2023-04-27 18:26:47 +0600

    aa

--- src/Agency/Services/agency.service.ts
+++ src/Agency/Services/agency.service.ts
@@ -4,130 +4,128 @@ import { Repository } from 'typeorm';
 import { AgencyEntity } from '../entities/agency.entity';
 import { AgencyDto } from '../DTOs/agency.dto';
 import * as bcrypt from 'bcrypt';
-import { MailerService } from "@nestjs-modules/mailer";
+import { MailerService } from '@nestjs-modules/mailer';
 
 @Injectable()
 export class AgencyService {
-    constructor(
-        @InjectRepository(AgencyEntity)
-        private agencyRepo: Repository<AgencyEntity>, private mailerService: MailerService
-    ) { }
+  constructor(
+    @InjectRepository(AgencyEntity)
+    private agencyRepo: Repository<AgencyEntity>,
+    private mailerService: MailerService,
+  ) {}
 
+  getIndex(): string {
+    return 'Welcome to Agency Dash Board';
+  }
 
-    getIndex(): string {
-        return "Welcome to Agency Dash Board";
+  getTmanagerProfilebyemail(Email): any {
+    return this.agencyRepo.findOneBy({ Email });
+  }
 
+  get(): any {
+    return this.agencyRepo.find();
+  }
+
+  async insert(agencyDTO: AgencyDto) {
+    const salt = await bcrypt.genSalt();
+    const hassedpassed = await bcrypt.hash(agencyDTO.password, salt);
+    agencyDTO.password = hassedpassed;
+    return this.agencyRepo.save(agencyDTO);
+  }
+
+  // async signin(mydto) {
+  // const mydata = await this.agencyRepo.findOneBy({ Email: mydto.Email });
+  // const isMatch = await bcrypt.compare(mydto.password, mydata.password);
+
+  // if (typeof isMatch !== 'undefined') {
+  // }
+  // if (isMatch) {
+  // return mydata.id;
+  // }
+  // else {
+  // return 0;
+  // }
+  // }
+
+  async signin(Email: string, password: string) {
+    const mydata = await this.agencyRepo.findOneBy({ Email: Email });
+    const isMatch = await bcrypt.compare(password, mydata.password);
+
+    if (isMatch) {
+      return 1;
+    } else {
+      return 0;
     }
+  }
 
-    get(): any {
-        return this.agencyRepo.find();
+  async update(agencyDTO: AgencyDto, id) {
+    const salt = await bcrypt.genSalt();
+    const hassedpassed = await bcrypt.hash(agencyDTO.password, salt);
+    agencyDTO.password = hassedpassed;
+    return this.agencyRepo.update(id, agencyDTO);
+  }
+
+  getAgencyById(id) {
+    return this.agencyRepo.findOneBy({ id });
+  }
+
+  SearchAgencyByName(AgencyName) {
+    return this.agencyRepo.findOneBy({ AgencyName });
+  }
+
+  deleteAgencyByid(id) {
+    return this.agencyRepo.delete(id);
+  }
+
+  // async sendEmail(mydata) {
+  //     return await this.mailerService.sendMail({
+  //         to: mydata.email,
+  //         subject: mydata.subject,
+  //         text: mydata.text,
+  //     });
+
+  // }
+  async sendEmail(mydata, file) {
+    if (!mydata.email) {
+      throw new Error('Recipient email address is missing');
     }
-
-    async insert(agencyDTO: AgencyDto) {
-
-        const salt = await bcrypt.genSalt();
-        const hassedpassed = await bcrypt.hash(agencyDTO.password, salt);
-        agencyDTO.password = hassedpassed;
-        return this.agencyRepo.save(agencyDTO);
-
+    const attachments = [];
+    if (file) {
+      attachments.push({
+        filename: file.originalname,
+        content: file.buffer, //temporary storage area for data in memory.
+        encoding: 'base64', //Base64 encoding is commonly used to transmit binary data over text-based protocols
+      });
     }
+    return await this.mailerService.sendMail({
+      to: mydata.email,
+      subject: mydata.subject,
+      text: mydata.text,
+      attachments: attachments,
+    });
+  }
 
-    async signin(mydto) {
-        const mydata = await this.agencyRepo.findOneBy({ Email: mydto.Email });
-        const isMatch = await bcrypt.compare(mydto.password, mydata.password);
+  FindFeedbacksByAgencyId(id): any {
+    return this.agencyRepo.find({
+      where: { id: id },
+      relations: {
+        feedBack: true,
+      },
+    });
+  }
 
-        if (typeof isMatch !== 'undefined') {
-        }
-        if (isMatch) {
-            return mydata.id;
-        }
-        else {
-            return 0;
-        }
-    }
+  FindTendersByAgencyId(id): any {
+    return this.agencyRepo.find({
+      where: { id: id },
+      relations: {
+        tenders: true,
+      },
+    });
+  }
 
-
-    async update(agencyDTO: AgencyDto, id) {
-
-        const salt = await bcrypt.genSalt();
-        const hassedpassed = await bcrypt.hash(agencyDTO.password, salt);
-        agencyDTO.password = hassedpassed;
-        return this.agencyRepo.update(id, agencyDTO);
-    }
-
-    getAgencyById(id) {
-
-        return this.agencyRepo.findOneBy({ id });
-    }
-
-
-    SearchAgencyByName(AgencyName) {
-        return this.agencyRepo.findOneBy({ AgencyName });
-    }
-
-    deleteAgencyByid(id) {
-        return this.agencyRepo.delete(id);
-
-    }
-
-    // async sendEmail(mydata) {
-    //     return await this.mailerService.sendMail({
-    //         to: mydata.email,
-    //         subject: mydata.subject,
-    //         text: mydata.text,
-    //     });
-
-    // }
-    async sendEmail(mydata, file){
-        if (!mydata.email) {
-          throw new Error('Recipient email address is missing');
-        }
-        const attachments = [];
-        if (file) {
-          attachments.push({
-            filename: file.originalname,
-            content: file.buffer,//temporary storage area for data in memory.
-            encoding: 'base64' //Base64 encoding is commonly used to transmit binary data over text-based protocols 
-          });
-        }
-        return await this.mailerService.sendMail({
-          to: mydata.email,
-          subject: mydata.subject,
-          text: mydata.text,
-          attachments: attachments
-        });
-      }
-
-
-    FindFeedbacksByAgencyId(id): any {
-        return this.agencyRepo.find(({
-            where: { id: id },
-            relations: {
-                feedBack: true,
-            },
-        }))
-    }
-
-
-    FindTendersByAgencyId(id): any {
-        return this.agencyRepo.find(({
-            where: { id: id },
-            relations: {
-                tenders: true,
-            },
-        }))
-    }
-
-    ChangeStatus(id, status) {
-        return this.agencyRepo.update(id, {
-            Status: status
-        })
-    }
-
-
-
-
-
-
-
+  ChangeStatus(id, status) {
+    return this.agencyRepo.update(id, {
+      Status: status,
+    });
+  }
 }
--- src/Agency/agency.controller.ts
+++ src/Agency/agency.controller.ts
@@ -1,4 +1,23 @@
-import { Body, Controller, Delete, Get, Param, ParseBoolPipe, ParseIntPipe, Patch, Post, Put, Query, Res, Session, UploadedFile, UseGuards, UseInterceptors, UsePipes, ValidationPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseBoolPipe,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Put,
+  Query,
+  Res,
+  Session,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { AgencyEntity } from './entities/agency.entity';
 import { AgencyService } from './Services/agency.service';
 import { AgencyDto } from './DTOs/agency.dto';
@@ -14,30 +33,38 @@ import { TenderService } from 'src/tender-manager/Services/tender.service';
 import { TenderAuctinForm } from 'src/tender-manager/DTOs/TenderAuction.dto';
 import { TenderAuctionService } from 'src/tender-manager/Services/tenderAuction.service';
 
-@Controller("Agency")
+@Controller('Agency')
 export class AgencyController {
-  constructor(private tenderauctionService: TenderAuctionService,private readonly agencyService: AgencyService, private readonly BudgetReqService: BudgetReqService,private readonly tenderservice: TenderService) { }
+  constructor(
+    private tenderauctionService: TenderAuctionService,
+    private readonly agencyService: AgencyService,
+    private readonly BudgetReqService: BudgetReqService,
+    private readonly tenderservice: TenderService,
+  ) {}
 
   @UseGuards(SessionGuard)
-  @Get("/index")
+  @Get('/index')
   getAdmin(@Session() session): any {
     console.log(session.agencyid);
     return this.agencyService.getIndex();
   }
 
-  @Get("/viewprofile/:id")
-  getUserByID(@Param("id", ParseIntPipe) id: number): any {
+  @Get('/viewprofile/:id')
+  getUserByID(@Param('id', ParseIntPipe) id: number): any {
     return this.agencyService.getAgencyById(id);
   }
 
-  @Post("/signup")
+  @Post('/signup')
   @UsePipes(new ValidationPipe())
   @UseInterceptors(FileInterceptor('file', { dest: 'tmp/' }))
-  async create(@UploadedFile() file: Express.Multer.File, @Body() tmdto: AgencyDto) {
-
-
+  async create(
+    @UploadedFile() file: Express.Multer.File,
+    @Body() tmdto: AgencyDto,
+  ) {
     if (file) {
-      const filename = `${moment().format('YYYYMMDDHHmmss')}${extname(file.originalname)}`;
+      const filename = `${moment().format('YYYYMMDDHHmmss')}${extname(
+        file.originalname,
+      )}`;
       tmdto.ImgfileName = filename;
       const tmpFilePath = file.path; // temporary path of the uploaded file
       const destFilePath = `Images/${filename}`;
@@ -48,20 +75,51 @@ export class AgencyController {
     return await this.agencyService.insert(tmdto);
   }
 
-  @Get('/signin')
-  async signin(@Session() session, @Body() mydto: AgencyDto) {
+  // @Get('/signin')
+  // async signin(@Session() session, @Body() mydto: AgencyDto) {
 
-    var id = await this.agencyService.signin(mydto);
-    if (id) {
+  // var id = await this.agencyService.signin(mydto);
+  // if (id) {
 
-      session.agencyid = id;
-      return { message: "Login Success !" };
-    }
-    else {
-      return { message: "invalid credentials" };
-    }
+  // session.agencyid = id;
+  // return { message: "Login Success !" };
+  // }
+  // else {
+  // return { message: "invalid credentials eee" };
+  // }
 
-  }
+  // }
+
+
+
+  
+  @Post('/signin')
+  async signin(@Session() session, @Body('Email') email: string, @Body('password') password: string) {
+    
+console.log(email,password);
+     var b=await this.agencyService.signin(email, password);
+     if (b) {
+         session.email = email;
+         return session.email;
+     } else {        
+         return 0;
+     }
+ }
+
+  // @Get('/signin')
+  // async signin(
+    // @Session() session,
+    // @Body('Email') Email: string,
+    // @Body('password') password: string,
+  // ) {
+    // var b = await this.agencyService.signin(Email, password);
+    // if (b) {
+      // session.email = Email;
+      // return session.email;
+    // } else {
+      // return { message: 'invalid credentials eee' };
+    // }
+  // }
 
   @Get('/signout')
   signout(@Session() session, @Res() res: Response) {
@@ -69,12 +127,14 @@ export class AgencyController {
       if (err) {
         throw new Error('Failed to destroy session');
       }
-      res.setHeader('Set-Cookie', ['connect.sid=; Max-Age=-1; Path=/; HttpOnly']);
+      res.setHeader('Set-Cookie', [
+        'connect.sid=; Max-Age=-1; Path=/; HttpOnly',
+      ]);
       res.status(200).json({ message: 'Logged out successfully' });
     });
   }
 
-  @Put("/update/:id")
+  @Put('/update/:id')
   @UsePipes(new ValidationPipe())
   async update(@Body() admindto: AgencyDto, @Param('id') id: number) {
     return this.agencyService.update(admindto, id);
@@ -86,92 +146,86 @@ export class AgencyController {
   // }
   @Post('/sendemail')
   @UseInterceptors(FileInterceptor('file'))
-  async sendEmail(@Body() mydata, @UploadedFile() file){
-      return await this.agencyService.sendEmail(mydata, file);
+  async sendEmail(@Body() mydata, @UploadedFile() file) {
+    return await this.agencyService.sendEmail(mydata, file);
   }
-
-
-
 
   //--BudgetRequest
 
-  @Post("/BudgetReq/create")
+  @Post('/BudgetReq/create')
   @UsePipes(new ValidationPipe())
   createTender(@Body() tenderdto: BudgeRequestDto): any {
     return this.BudgetReqService.insert(tenderdto);
   }
 
-  @Put("/BudgetReq/update/:id")
+  @Put('/BudgetReq/update/:id')
   @UsePipes(new ValidationPipe())
   async updateTender(@Body() tdto: BudgeRequestDto, @Param('id') id: number) {
     return this.BudgetReqService.update(tdto, id);
   }
 
-  @Delete("/BudgetReq/delete/:id")
-  deleteTenderById(@Param("id", ParseIntPipe) id: number): any {
+  @Get('/viewprofilebyemail/:Email')
+  getUserByemail(@Param('Email') Email: string): any {
+    return this.agencyService.getTmanagerProfilebyemail(Email);
+  }
+
+  @Delete('/BudgetReq/delete/:id')
+  deleteTenderById(@Param('id', ParseIntPipe) id: number): any {
     return this.BudgetReqService.deleteBudgetReqById(id);
   }
 
-  @Get("/BudgetReq/all")
+  @Get('/BudgetReq/all')
   getAllTender(): any {
     return this.BudgetReqService.getAll();
   }
 
-  @Get("/BudgetReq/view/:id")
-  GetBudgetReqById(@Param("id", ParseIntPipe) id: number): any {
+  @Get('/BudgetReq/view/:id')
+  GetBudgetReqById(@Param('id', ParseIntPipe) id: number): any {
     return this.BudgetReqService.get(id);
   }
- 
-
- 
-
 
   //FeedBacks & Prev Records
 
-  @Get("/Feedbacks/:id")
-  GetFeedbacksById(@Param("id", ParseIntPipe) id: number): any {
+  @Get('/Feedbacks/:id')
+  GetFeedbacksById(@Param('id', ParseIntPipe) id: number): any {
     return this.agencyService.FindFeedbacksByAgencyId(id);
   }
 
-  @Get("/PrevProjects/:id")
-  GetPrevProjectsById(@Param("id", ParseIntPipe) id: number): any {
+  @Get('/PrevProjects/:id')
+  GetPrevProjectsById(@Param('id', ParseIntPipe) id: number): any {
     return this.agencyService.FindTendersByAgencyId(id);
   }
 
-  @Get("/TotalProjectCompleted/:id")
-  GetTotalProjectCompletedById(@Param("id", ParseIntPipe) id: number): any {
-    return this.tenderservice.getTotalTenderscompletedByAgencyID(3,id);
+  @Get('/TotalProjectCompleted/:id')
+  GetTotalProjectCompletedById(@Param('id', ParseIntPipe) id: number): any {
+    return this.tenderservice.getTotalTenderscompletedByAgencyID(3, id);
   }
-
 
   //Auctions
-  @Post("/Auction/createbid")
+  @Post('/Auction/createbid')
   @UsePipes(new ValidationPipe())
   createBid(@Body() TaucDTO: TenderAuctinForm): any {
-      return this.tenderauctionService.insert(TaucDTO);
+    return this.tenderauctionService.insert(TaucDTO);
   }
 
-  @Put("/Auction/updatebid:id")
+  @Put('/Auction/updatebid:id')
   @UsePipes(new ValidationPipe())
   async updateBid(@Body() tdto: TenderAuctinForm, @Param('id') id: number) {
-      return this.tenderauctionService.update(tdto, id);
+    return this.tenderauctionService.update(tdto, id);
   }
 
-  @Delete("/Auction/deletebid/:id")
-  deleteBid(@Param("id", ParseIntPipe) id: number): any {
-      return this.tenderauctionService.deleteBidById(id);
+  @Delete('/Auction/deletebid/:id')
+  deleteBid(@Param('id', ParseIntPipe) id: number): any {
+    return this.tenderauctionService.deleteBidById(id);
   }
 
-  @Get("/Auction/allbid/:id")
-  getAllBids(@Param("id", ParseIntPipe) id: number): any {
-      return this.tenderauctionService.getAll(id);
+  @Get('/Auction/allbid/:id')
+  getAllBids(@Param('id', ParseIntPipe) id: number): any {
+    return this.tenderauctionService.getAll(id);
   }
 
-  @Get("/Auction/viewBid/:id")
-  getBidByID(@Param("id", ParseIntPipe) id: number): any {
-      return this.tenderauctionService.get(id);
+  @Get('/Auction/viewBid/:id')
+  getBidByID(@Param('id', ParseIntPipe) id: number): any {
+    return this.tenderauctionService.get(id);
   }
-
-
 }
-
